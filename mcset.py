6588c24fd7de70f8558923b9bbb8b502aa68e1f6
INADA Naoki <inada-n@klab.com>
2012-11-01 16:07:38 +0900

    set test data to memcache

---
+++ mcset.py
@@ -0,0 +1,30 @@
+import socket
+
+s = socket.socket()
+s.connect(('127.0.0.1', 11211))
+
+def mcset(key, val):
+    s.sendall("set %s 0 0 %d\r\n%s\r\n" % (key, len(val), val))
+
+mcset('/foo', """\
+text/plain\r
+fizzbuzz
+""")
+
+mcset('/bar', """\
+text/html\r
+1
+2
+<!--# include fizz -->
+4
+<!--# include buzz -->
+<!--# include fizz -->
+7
+8
+<!--# include fizz -->
+<!--# include buzz -->
+11
+""")
+
+mcset('fizz', 'FIZZ\n')
+mcset('buzz', 'FIZZ\n')
